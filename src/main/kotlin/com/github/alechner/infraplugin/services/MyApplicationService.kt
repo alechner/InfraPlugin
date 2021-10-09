3c2491c53446060458879ed779981a6bf4c1ad83
GitHub Action <action@github.com>
2021-10-09 19:08:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/alechner/infraplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alechner/infraplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alechner.infraplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alechner.infraplugin.MyBundle
 
 class MyApplicationService {
 
